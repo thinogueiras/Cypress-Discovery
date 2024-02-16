1f10622f74156ec2fa64a856c7f6bc9d70e83f5c
Thiago Nogueira dos Santos <thiagonogueira@hotmail.com.br>
2024-02-16 15:29:57 -0300

    Novos testes adicionados

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -17,10 +17,6 @@ describe('Signup page', () => {
         const expectMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.';
 
         signup.modalContentShouldBe(expectMessage);
-
-        const btnFechar = '.swal2-popup.swal2-modal.swal2-icon-success.swal2-show button[type="button"].swal2-confirm';
-
-        signup.modalCloseAndValidateMessage(btnFechar);
     });
 
     it('Should validate invalid user CPF', () => {
@@ -40,6 +36,21 @@ describe('Signup page', () => {
         signup.submit();
         signup.alertMessageShouldBe('Oops! Email com formato inválido.');
     });
+
+    it('Should validate invalid user WhatsApp number', () => {
+        deliver.whatsApp = '11@@999!!!';
+        signup.fillForm(deliver);
+        signup.clickDeliverVehicle(deliver.delivery_method);
+        signup.uploadCnh(deliver.cnh);
+        signup.submit();
+        signup.alertMessageShouldBe('Oops! Whatsapp com formato incorreto');
+    });
+
+    it('Should validate invalid user PostalCode number', () => {
+        deliver.address.postalCode = '123abc-000';
+        signup.clickOnPostalCodeButton();
+        signup.alertMessageShouldBe('Informe um CEP válido');
+    });
 });
 
 describe('Validate required fields', () => {
@@ -48,21 +59,19 @@ describe('Validate required fields', () => {
         signup.submit();
     });
 
-    context('Validate messages', () => {
-        const messages = [
-            { field: 'name', output: 'É necessário informar o nome' },
-            { field: 'cpf', output: 'É necessário informar o CPF' },
-            { field: 'email', output: 'É necessário informar o email' },
-            { field: 'postalCode', output: 'É necessário informar o CEP' },
-            { field: 'number', output: 'É necessário informar o número do endereço' },
-            { field: 'delivery_method', output: 'Selecione o método de entrega' },
-            { field: 'cnh', output: 'Adicione uma foto da sua CNH' },
-        ];
+    const messages = [
+        { field: 'name', output: 'É necessário informar o nome' },
+        { field: 'cpf', output: 'É necessário informar o CPF' },
+        { field: 'email', output: 'É necessário informar o email' },
+        { field: 'postalCode', output: 'É necessário informar o CEP' },
+        { field: 'number', output: 'É necessário informar o número do endereço' },
+        { field: 'delivery_method', output: 'Selecione o método de entrega' },
+        { field: 'cnh', output: 'Adicione uma foto da sua CNH' },
+    ];
 
-        messages.forEach((msg) => {
-            it(`${msg.field} is required`, async () => {
-                await signup.alertMessageShouldBe(msg.output);
-            });
+    messages.forEach((msg) => {
+        it(`${msg.field} is required`, () => {
+            signup.alertMessageContains(msg.output);
         });
     });
 });
