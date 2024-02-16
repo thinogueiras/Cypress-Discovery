f2de14a8a99dd0c09cc1c29e28f30fab63892a4d
Thiago Nogueira dos Santos <thiagonogueira@hotmail.com.br>
2024-02-16 15:28:57 -0300

    Novos testes adicionados

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,7 +1,21 @@
-describe('Home Page', () => {
-    it('Deve validar se o app está online', () => {
+describe('Home Page Tests', () => {
+    beforeEach(() => {
         cy.visit('/');
+    });
+
+    it('Deve validar o título', () => {
+        cy.title().should('eq', 'Buger Eats');
+    });
+
+    it('Deve validar o logo Buger Eats', () => {
         cy.get('#page-home .content img[alt*="Buger Eats"]');
+    });
+
+    it('Deve validar o texto do cabeçalho', () => {
         cy.get('#page-home .content h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats');
+    });
+
+    it('Deve validar o texto central', () => {
+        cy.get('#page-home .content p').should('have.text', 'Em vez de oportunidades tradicionais de entrega de refeições em horários pouco flexíveis, seja seu próprio chefe.');
     });
 });
